1d9ec96e666cff0b5c6739439376afdab8158d24
pavanpradeep2 <107210843+pavanpradeep2@users.noreply.github.com>
2022-12-02 17:46:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,9 +14,9 @@ class Config(object):
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Link Search Bot.
     
+    roiderff
     
-    
-🤖 My Name: <a href='https://youtube.com/@GreyMattersBot'>Link Search Bot</a>
+🤖 My Name: <a href='https://t.me/roiderff'>Link Search Bot</a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
@@ -24,10 +24,10 @@ class Config(object):
 
 📡 Server: <a href='koyeb.com'>Koyeb</a>
 
-👨‍💻 Created By: <a href='https://t.me/GreyMatter_Bot'>GreyMatter's Bot</a></b>
+👨‍💻 Created By: <a href='https://t.me/roiderff'>roider bots</a></b>
 """
 
-    ABOUT_HELP_TEXT = """<b>👨‍💻 Creator : <a href='https://t.me/GreyMatter_Bot'>GreyMatter's Bot</a>
+    ABOUT_HELP_TEXT = """<b>👨‍💻 Creator : <a href='https://t.me/roiderff'>roider's Bot</a>
 If You Want Your Own Bot Like This Then You Can Contact Our Creator.</b>
 """
 
@@ -38,7 +38,7 @@ I'm Link Search Bot.🤖
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @GreyMatter_Bots</a></b>
+<a>Made With ❤ By @roiderff</a></b>
 """
 
 
@@ -49,6 +49,6 @@ I'm Link Search Bot.🤖
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @GreyMatter_Bots</a></b>
+<a>Made With ❤ By @roiderff</a></b>
 """
 
